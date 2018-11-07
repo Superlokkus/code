10e8386715e1b90e4e23405abbf8bcbec493b5af
Markus Klemm <Markus.Klemm@logmein.com>
2018-11-07 11:21:21 +0100

    Strange feeling but seems better, but I recon just start implementing now

--- include/registry.hpp
+++ include/registry.hpp
@@ -18,13 +18,18 @@ using service_identifier = std::string;
 
 using object_identifier = boost::uuids::uuid;
 
-using incoming_callback = std::function<void (std::string)>;
+using incoming_callback = std::function<std::string(std::string)>;
+
+using service_factory_callback = std::function<void(object_identifier)>;
+
+class object_socket final {
+    std::future<void> send(std::string message);
+    std::future<void> recieve(incoming_callback incoming_message_callback);
+};
 
 class object_handle final {
 public:
-    std::future<void> send(std::string message);
-
-    std::future<void> recieve(incoming_callback incoming_message_callback);
+    object_socket socket;
 
 private:
     boost::uuids::uuid id;
@@ -35,16 +40,17 @@ private:
 class registry final {
 public:
 
-    std::future<void> register_service(service_identifier service_id,
-                                       incoming_callback incoming_message_callback);
+    std::future<void> register_stateless_service(service_identifier service_id,
+                                                 incoming_callback);
+
+    std::future<void> register_statefull_service(service_identifier service_id,
+                                                 service_factory_callback factory_callback);
 
     std::future<object_handle> use_service_interface(service_identifier service_id);
 
-    std::future<object_identifier> expose(service_identifier service_id,
-                                          incoming_callback incoming_message_callback);
+    std::future<object_identifier> expose(service_identifier service_id);
 
     std::future<object_handle> consume(object_identifier object);
-
 
 
 };
