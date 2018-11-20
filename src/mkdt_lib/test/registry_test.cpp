2e417fe26f7e88b4d4f194dc4e4b4a0b98913180
Markus Klemm <Markus.Klemm@logmein.com>
2018-11-20 13:57:14 +0100

    More restruc

--- src/mkdt_lib/test/registry_test.cpp
+++ src/mkdt_lib/test/registry_test.cpp
@@ -3,15 +3,10 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include <registry.hpp>
-#include <object_remoting_mockup.hpp>
 
 BOOST_AUTO_TEST_SUITE(registry_tests)
 
 BOOST_AUTO_TEST_CASE(FIRST_TEST) {
-    mkdt::object_remoting_mockup::client_stub client{};
-    mkdt::object_remoting_mockup::server_stub server{};
-
-
     BOOST_CHECK(true);
 }
 
